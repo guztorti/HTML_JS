714ff466cb5c8f08a2b2fb7f23a34db36e94fea5
Gustavo Osvaldo Torti <guztorti@gmail.com>
2022-06-30 20:37:58 -0300

    pa el juampi

--- javaScript/motor.js
+++ javaScript/motor.js
@@ -33,9 +33,9 @@ function cargar() {
                 alert(`correcto!!`);
                 break;
 
-            default:{
+            default: {
                 alert(`incorrecto`);
-                document.getElementById('txt4').value ='';
+                document.getElementById('txt4').value = '';
                 document.getElementById('txt4').focus;
             }
                 break;
@@ -46,155 +46,197 @@ function cargar() {
 
 }
 /*Ejercicio 5*/{
-function apretarNumero(numero){
-    if (PANTALLA.value.length === 12){
-        return;
+    function apretarNumero(numero) {
+        if (PANTALLA.value.length === 12) {
+            return;
+        }
+        if (calculado) {
+            PANTALLA.value = numero;
+            calculado = false;
+        } else if (PANTALLA.value == 0) {
+            PANTALLA.value = numero;
+        }
+        else {
+            PANTALLA.value += numero;
+        }
+        if (numero === ',') {
+            document.getElementById('btnPunto').disabled = true;
+        }
+        evaluarEsPar();
     }
-    if(calculado){
-        PANTALLA.value = numero;
+
+    let operando1 = 0;
+    let calculado = false;
+    let operador;
+
+    function calculadora() {
+        const num1 = document.getElementById('btn1');
+        const num2 = document.getElementById('btn2');
+        const num3 = document.getElementById('btn3');
+        const num4 = document.getElementById('btn4');
+        const num5 = document.getElementById('btn5');
+        const num6 = document.getElementById('btn6');
+        const num7 = document.getElementById('btn7');
+        const num8 = document.getElementById('btn8');
+        const num9 = document.getElementById('btn9');
+        const num0 = document.getElementById('btn0');
+        const numDot = document.getElementById('btnPunto');
+        const commEq = document.getElementById('btnIgual');
+        const commback = document.getElementById('btnAtras');
+        const commReset = document.getElementById('btnBorrar');
+        const commMultip = document.getElementById('btnMul');
+        const commDividir = document.getElementById('btnDiv');
+        const commSumar = document.getElementById('btnMas');
+        const commRestar = document.getElementById('btnMenos');
+
+        num1.onclik = apretarNumero(1);
+        num2.onclik = apretarNumero(2);
+        num3.onclik = apretarNumero(3);
+        num4.onclik = apretarNumero(4);
+        num5.onclik = apretarNumero(5);
+        num6.onclik = apretarNumero(6);
+        num7.onclik = apretarNumero(7);
+        num8.onclik = apretarNumero(8);
+        num9.onclik = apretarNumero(9);
+        num0.onclik = apretarNumero(0);
+        numDot.onclik = apretarNumero();
+        commEq.onclik = calcular(operador);
+        commback = atras();
+        commReset = borrar();
+        commMultip = multiplicar();
+        commDividir = dividir();
+        commSumar = sumar();
+        commRestar = restar();
+
+    }
+    function borrar() {
+        operando1 = 0;
         calculado = false;
-    }else if (PANTALLA.value == 0){
-        PANTALLA.value = numero;
-        }
-        else{
-        PANTALLA.value +=numero;
-        }
-    if (numero === ','){
-        document.getElementById('btnPunto').disabled = true;
-    }
-    evaluarEsPar();
-}
-
-let operando1 = 0;
-let calculado = false;
-let operador;
-
-function calculadora(){
-    const num1 = document.getElementById('btn1');
-    const num2 = document.getElementById('btn2');
-    const num3 = document.getElementById('btn3');
-    const num4 = document.getElementById('btn4');
-    const num5 = document.getElementById('btn5');
-    const num6 = document.getElementById('btn6');
-    const num7 = document.getElementById('btn7');
-    const num8 = document.getElementById('btn8');
-    const num9 = document.getElementById('btn9');
-    const num0 = document.getElementById('btn0');
-    const numDot = document.getElementById('btnPunto');
-    const commEq = document.getElementById('btnIgual');
-    const commback = document.getElementById('btnAtras');
-    const commReset = document.getElementById('btnBorrar');
-    const commMultip = document.getElementById('btnMul');
-    const commDividir = document.getElementById('btnDiv');
-    const commSumar = document.getElementById('btnMas');
-    const commRestar = document.getElementById('btnMenos');    
-
-    num1.onclik = apretarNumero(1);
-    num2.onclik = apretarNumero(2);
-    num3.onclik = apretarNumero(3);
-    num4.onclik = apretarNumero(4);
-    num5.onclik = apretarNumero(5);
-    num6.onclik = apretarNumero(6);
-    num7.onclik = apretarNumero(7);
-    num8.onclik = apretarNumero(8);
-    num9.onclik = apretarNumero(9);
-    num0.onclik = apretarNumero(0);
-    numDot.onclik = apretarNumero();
-    commEq.onclik = calcular(operador);
-    commback = atras();
-    commReset = borrar();
-    commMultip = multiplicar();
-    commDividir = dividir();
-    commSumar = sumar();
-    commRestar = restar();
-
-}
-function borrar(){
-    operando1 = 0;
-    calculado = false;
-    PANTALLA.value = 0;
-}
-
-function atras(){
-
-    if(PANTALLA.value.length>0){
-    PANTALLA.value = String(PANTALLA.value).substring(0,PANTALLA.value.length-1)
-    }else{
         PANTALLA.value = 0;
     }
-    evaluarEsPar();
-}
-function calcular(){
-    switch(operador){
-        case '+':
-            PANTALLA.value = operando1 + Number(PANTALLA.value);
-           
-            break;
-        case '-':
-            PANTALLA.value = operando1 - Number(PANTALLA.value);
-            break; 
-        case '*':
-            PANTALLA.value = operando1 * Number(PANTALLA.value);
-            break;
-        case '/':
-            PANTALLA.value = operando1 / Number(PANTALLA.value);
-            break; 
-    }
-    calculado = true;
-    evaluarEsPar();
-}
 
-function sumar(){
-    operando1 += Number(PANTALLA.value);
-    operador = '+';
-    PANTALLA.value = operando1;
-    calculado = true;
-    evaluarEsPar();
-}
-function multiplicar(){
-    if(operando1 == 0){operando1 = 1}
-    operando1 *= Number(PANTALLA.value);
-    operador = '*';
-    PANTALLA.value = operando1;
-    calculado = true;
-    evaluarEsPar();
-}
-function restar(){
-    if(calculado){
-        operando1 = Number(PANTALLA.value)*-1;
-    }else {
-    operando1 = Number(PANTALLA.value);
-    operador = '-';
-    PANTALLA.value = operando1;
-    calculado = true;
+    function atras() {
+
+        if (PANTALLA.value.length > 0) {
+            PANTALLA.value = String(PANTALLA.value).substring(0, PANTALLA.value.length - 1)
+        } else {
+            PANTALLA.value = 0;
+        }
+        evaluarEsPar();
     }
-    evaluarEsPar()
-}
-function dividir(){
-    operando1 = Number(PANTALLA.value);
-    operador = '/';
-    PANTALLA.value = operando1;
-    calculado = true;
-    evaluarEsPar();
-} 
+    function calcular() {
+        switch (operador) {
+            case '+':
+                PANTALLA.value = operando1 + Number(PANTALLA.value);
+
+                break;
+            case '-':
+                PANTALLA.value = operando1 - Number(PANTALLA.value);
+                break;
+            case '*':
+                PANTALLA.value = operando1 * Number(PANTALLA.value);
+                break;
+            case '/':
+                PANTALLA.value = operando1 / Number(PANTALLA.value);
+                break;
+        }
+        calculado = true;
+        evaluarEsPar();
+    }
+
+    function sumar() {
+        operando1 += Number(PANTALLA.value);
+        operador = '+';
+        PANTALLA.value = operando1;
+        calculado = true;
+        evaluarEsPar();
+    }
+    function multiplicar() {
+        if (operando1 == 0) { operando1 = 1 }
+        operando1 *= Number(PANTALLA.value);
+        operador = '*';
+        PANTALLA.value = operando1;
+        calculado = true;
+        evaluarEsPar();
+    }
+    function restar() {
+        if (calculado) {
+            operando1 = Number(PANTALLA.value) * -1;
+        } else {
+            operando1 = Number(PANTALLA.value);
+            operador = '-';
+            PANTALLA.value = operando1;
+            calculado = true;
+        }
+        evaluarEsPar()
+    }
+    function dividir() {
+        operando1 = Number(PANTALLA.value);
+        operador = '/';
+        PANTALLA.value = operando1;
+        calculado = true;
+        evaluarEsPar();
+    }
 }
 /* Ejercicio 6 */{
-    function evaluarEsPar(){
-        if(PANTALLA.value%2===0){
+    function evaluarEsPar() {
+        if (PANTALLA.value % 2 === 0) {
             document.querySelector('#esPar').textContent = "El número en la pantalla es PAR";
-        }else {
+        } else {
             document.querySelector('#esPar').textContent = "El número en la pantalla es IMPAR";
         }
     }
 
 
 /* Ejercicio 10 */ {
-//let palabra = prompt("Ingrese palabra a invertir");
-//let invertida = (palabra)=> console.log(palabra.split('').reverse().join(''));
-//invertida(palabra);
-}
+        //let palabra = prompt("Ingrese palabra a invertir");
+        //let invertida = (palabra)=> console.log(palabra.split('').reverse().join(''));
+        //invertida(palabra);
+    }
 }
 /* Ejercicio 12 */{
     let tipoDeDato = (argumento) => console.log(typeof argumento);
-    tipoDeDato(new Set([2,2,3,3,4,4,4]))
+    tipoDeDato(new Set([2, 2, 3, 3, 4, 4, 4]))
+}
+/**A partir del siguiente array: var valores = [true, 5, false, "hola", "adios", 2]:
+a) Determinar cual de los dos elementos de texto es mayor
+b) Utilizando exclusivamente los dos valores booleanos del array, determinar los
+operadores necesarios para obtener un resultado true y otro resultado false
+c) Determinar el resultado de las cinco operaciones matemáticas realizadas con los
+dos elementos numéricos */
+/* Ejercicio 18  */{
+    let valores = [true, 5, false, "hola", "adios", 2];
+    function determinarTextoMayor() {
+        var aux = '';
+        for (var i = 0; i < valores.length; i++) {
+            if (typeof valores[i] === 'string') {
+                if (aux === '') {
+                    aux = valores[i];
+                }
+                if (aux.length < valores[i].length) {
+                    aux = valores[i];
+                }
+            }
+        }
+        return aux;
+    }
+    function buscarBooleano() {
+        let primer = 0;
+        let segundo;
+        for (var i = 0; i < valores.length; i++) {
+            if (typeof valores[i] === 'boolean') {
+                if (primer === 0) {
+                    primer = valores[i];
+                    
+                }else
+
+                segundo = valores[i];
+
+
+            }
+            return [primer || segundo, primer && segundo];
+        }
+    }
+    console.log(`el valor de texto más largo es: ${determinarTextoMayor()}`);
+    console.log(buscarBooleano());
 }
